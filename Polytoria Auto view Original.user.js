4d7c2da0e3652995758b5b948f60dd20bd33b37f
ㅤㅤㅤ <64054647+catgirIz@users.noreply.github.com>
2023-07-16 16:15:16 -0400

    Create Polytoria Auto view Original.user.js

---
+++ Polytoria Auto view Original.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Polytoria Auto view Original
+// @icon         https://cdn.discordapp.com/attachments/898314728219242597/900157044009611284/image.png
+// @license      Apache-2.0
+// @namespace    lol
+// @version      1.0.0
+// @description  Auto view Original for forum posts
+// @author       levisurely
+// @match        https://polytoria.com/forum/post/*
+// @match        http://polytoria.com/forum/post/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+document.querySelector("#c93e2206-2f31-4ddc-9828-2bb8e8ed940e").click()
+})();
